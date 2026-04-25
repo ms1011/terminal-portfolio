8668ef743801206f9ed8996c7d90c1a5cdf712eb
ms1011 <wkdalstjr94@gmail.com>
2026-04-26 01:47:28 +0900

    build: add spring-boot-starter-mail and mockito-kotlin

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,10 +22,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
